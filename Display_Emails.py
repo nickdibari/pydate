ceb0dd529e370a5d900b7b2882a5c1995eab2e07
nickdibari <ndibari@fordham.edu>
2016-09-23 14:38:35 -0400

    Added Display_Emails

---
+++ Display_Emails.py
@@ -0,0 +1,46 @@
+#* PYTHON EMAIL PROJECT                 *#
+#* Get_Emails.py                        *#
+#* Fordham CSS September 25             *#
+#* ------------------------------------ *#
+#* Script to print Emails from Database *#
+
+#IMPORTS
+from Email_Object import Email # Email class to be used
+
+import shelve				   # Database Management
+
+# PRE: Databse exists
+# POST: Database connection (db)
+# TODO: Clean up database here?
+#		-> Remove events that have already passed
+def Get_Connection():
+	pass
+
+# PRE: Databse connection
+# POST: Emails displayed
+def Print(db):
+	pass
+
+# PRE: Databse connection
+# POST: Display Emails that fit criteria
+def Search(db):
+	pass
+
+# PRE: Database connection
+# POST: Selected Emails deleted from Database
+def Delete(db):
+	pass
+
+# PRE: Database connection
+# POST: Database connection closed
+def Close_Connection(db):
+	pass
+
+# Main Prompt for User Interface
+def Prompt(db):
+	pass
+
+if __name__ == '__main__':
+	db = Get_Connection()
+	Prompt(db)
+	Close_Connection(db)
