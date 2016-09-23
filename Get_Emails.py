3ba2e67a710fd02a354821c6ca349470beaa6828
nickdibari <ndibari@fordham.edu>
2016-09-23 14:18:32 -0400

    Cleanup

--- Get_Emails.py
+++ Get_Emails.py
@@ -2,7 +2,7 @@
 
 #* PYTHON EMAIL PROJECT                 *#
 #* Get_Emails.py                        *#
-#* Ruhling, DiBari, Giroux             *#
+#* Fordham CSS September 25             *#
 #* ------------------------------------ *#
 #* Script to gather emails from server. *#
 
@@ -11,37 +11,34 @@ from Email_Object import Email # Email class to be used
 import imaplib                 # Network Connection
 import email                   # Email Parsing 
 import getpass                 # Password Protection    
-import re                      # Regular Expression for Matching Targets
+import re                      # Regular Expressions 
 
-#* -----GIROUX SECTION----------------- *#
 # PRE: Login Information from User
 # POST: Connection to EMail Sever
+# TODO: How to create connection to server?
+# TODO: How to store user login?
 def Set_Connection():
-    print("In Set_Connection")
+    pass
     
 # PRE: Connection with Server
 # POST: List of Emails to Prioritize
 def Get_Emails(RAW_EMAILS):
-    print("In Get_Emails")
+    pass
 
-#* -----RUHLING SECTION--------------- *#
 # PRE: List of Targeted Emails
 # POST: Emails stored in Database 
 def Store_Emails(Target_Emails):
-    print("In Store_Emails")
+    pass
     
-#* -----DIBARI SECTION----------------- *#
-# Set_Priority
-# PRE: One list of Email objects 
 # POST: Two list of Email objects
 #       High_Priority contains emails from important sender and important emails
 #       Low_Priority contains all other emails
 def Set_Priority(EMAIL_LIST):
-    print("In Set_Priority")
+    pass
     
     # Loop through all emails
     for a in range(len(EMAIL_LIST)):
-        print("In For Loop of Set_Priority")
+        pass
         # If the sender matches a sender in the user defined priority list
         # OR
         # If the sender address is from @fordham.edu
@@ -50,9 +47,14 @@ def Set_Priority(EMAIL_LIST):
         # Else
         # Then add to Low_Priority
 
-# Get_Targets
 # PRE: One list of prioritized Emails to parse
 # POST: One list of emails whose contents match common phrases for event sensitive emails
 def Get_Targets(priority_list, Is_High_Priority):
-    print("In Get_Targets")
+    pass
     
+# Main Driver
+def Main():
+    pass
+
+if __name__ == '__main__':
+    Main()
