38265df2e2292bf2953d4b383900a7611d9af784
sbrown92 <sbrown92@fordham.edu>
2016-10-03 16:35:48 -0400

    Update Get_Emails.py

--- Get_Emails.py
+++ Get_Emails.py
@@ -38,24 +38,60 @@ def DB_Connect():
 
 # PRE: Connection with Server
 # POST: List of Emails to Prioritize
+# PRE: Connection with Server
+# POST: List of Emails to Prioritize 
 def Get_Emails(conx):
+    Emails = []
     try:
-        rsp, box = conx.select('INBOX', readonly=True)
-        #TODO: change the search parameters to suite your own testing. 
-        rsp, msg = conx.search(None, '(FROM "Nicholas DiBari" UNSEEN)')
-        if(rsp == 'OK'):
-            for ids in msg:
-                rsp, msg_data = conx.fetch(ids, '(RFC822)')
-                for rsp_part in msg_data:
-                    if isinstance(rsp_part, tuple):
-                        msg = email.message_from_string(rsp_part[1])
-                        if(msg.is_multipart() == True):
-                            print "Yes"
-                        else:
-                            print "Not"
+        rsp, box = conx.select('INBOX', readonly=True)                                  # <--- TODO: Possibly extend to search other/all mailboxes.
+        if(rsp == 'OK'): 
+            rsp, msg = conx.search(None, '(UNSEEN)')                                    # msg[0] is a string of the message IDs found in the search
+            
+            if(rsp == 'OK'):
+                Msg_list = reversed(msg[0].split())                                     # The string is split and reversed so Msg_list contains a set 
+                                                                                        # starting with the newest email.
+
+                for ids in Msg_list:
+                    rsp, Msg_data = conx.fetch(ids, '(RFC822)')                         # Returns a response code and a set of the emails 
+                                                                                        # of the provided IDs in RFC2822 format.
+
+                    if(rsp == 'OK'):
+                        for rsp_part in Msg_data:
+                            if isinstance(rsp_part, tuple):
+                                msg = email.message_from_string(rsp_part[1])            #Create a email.message object from a string. 
+                                tempEmail = Email()
+
+
+
+                                tempEmail.subject = msg['subject']                      # **********************************************************
+                                tempEmail.sender = msg['from']                          # ** Populate the variables of the temporary Email object ** 
+                                tempEmail.date = msg['date']                            # **********************************************************
+
+
+                                if(msg.is_multipart() == True):                         # If the email message is a multi-part email you must get the 
+                                    bodyText = msg.get_payload(0)                       # get the payload as a string so it can be decoded. 
+                                    tempEmail.body = bodyText.get_payload(decode=True)
+                                else:
+                                    bodyText = msg.get_payload(decode=True)
+                                    if type(bodyText) is str:
+                                        tempEmail.body = bodyText
+                                    else:
+                                        raise Exception('Single-part Payload not working')  # TODO: Further testing on single-part emails. 
+                                
+
+                                Emails.append(tempEmail)
+                                if(len(Emails) == 100):                                     # The set of Email objects is set to return at 100 because 
+                                    return Emails                                           # of an issue with timing out. 
+
+
     except Exception as e:    
-        print('ERROR')
+        print('ERROR')                                                                      # TODO: Better exception handling.
         print e
+        
+        
+        
+        
+        
 # PRE: List of Targeted Emails
 # POST: Emails stored in Database 
 def Store_Emails(Target_Emails):
