faf9b6422275f992e8ba86f524622683dcbfd198
nickdibari <ndibari@fordham.edu>
2016-10-03 20:13:17 -0400

    Syntax formatting

--- Get_Emails.py
+++ Get_Emails.py
@@ -42,47 +42,37 @@ def DB_Connect():
 def Get_Emails(conx):
     Emails = []
     try:
-        rsp, box = conx.select('INBOX', readonly=True)                                  # <--- TODO: Possibly extend to search other/all mailboxes.
+        # TODO: Possibly extend to search other/all mailboxes.
+        rsp, box = conx.select('INBOX', readonly=True)
         if(rsp == 'OK'): 
-            rsp, msg = conx.search(None, '(UNSEEN)')                                    # msg[0] is a string of the message IDs found in the search
-            
+            rsp, msg = conx.search(None, '(UNSEEN)')           
             if(rsp == 'OK'):
-                Msg_list = reversed(msg[0].split())                                     # The string is split and reversed so Msg_list contains a set 
-                                                                                        # starting with the newest email.
-
+                Msg_list = reversed(msg[0].split())
                 for ids in Msg_list:
-                    rsp, Msg_data = conx.fetch(ids, '(RFC822)')                         # Returns a response code and a set of the emails 
-                                                                                        # of the provided IDs in RFC2822 format.
+                    rsp, Msg_data = conx.fetch(ids, '(RFC822)')
 
                     if(rsp == 'OK'):
                         for rsp_part in Msg_data:
                             if isinstance(rsp_part, tuple):
-                                msg = email.message_from_string(rsp_part[1])            #Create a email.message object from a string. 
+                                msg = email.message_from_string(rsp_part[1]) 
                                 tempEmail = Email()
-
-
-
-                                tempEmail.subject = msg['subject']                      # **********************************************************
-                                tempEmail.sender = msg['from']                          # ** Populate the variables of the temporary Email object ** 
-                                tempEmail.date = msg['date']                            # **********************************************************
-
-
-                                if(msg.is_multipart() == True):                         # If the email message is a multi-part email you must get the 
-                                    bodyText = msg.get_payload(0)                       # get the payload as a string so it can be decoded. 
+                                tempEmail.subject = msg['subject']
+                                tempEmail.sender = msg['from']
+                                tempEmail.date = msg['date']
+                                if(msg.is_multipart() == True):
+                                    bodyText = msg.get_payload(0) 
                                     tempEmail.body = bodyText.get_payload(decode=True)
                                 else:
                                     bodyText = msg.get_payload(decode=True)
                                     if type(bodyText) is str:
                                         tempEmail.body = bodyText
                                     else:
-                                        raise Exception('Single-part Payload not working')  # TODO: Further testing on single-part emails. 
-                                
-
+                                        # TODO: Further testing on single-part emails. 
+                                        raise Exception('Single-part Payload not working')
+                            
                                 Emails.append(tempEmail)
-                                if(len(Emails) == 100):                                     # The set of Email objects is set to return at 100 because 
-                                    return Emails                                           # of an issue with timing out. 
-
-
+                                if(len(Emails) == 100): 
+                                    return Emails
     except Exception as e:    
         print('ERROR')                                                                      # TODO: Better exception handling.
         print e
