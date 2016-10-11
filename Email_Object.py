6a288d35e69d896c8aafe74c5454b9f6b30f7266
nickdibari <ndibari@fordham.edu>
2016-10-11 15:19:04 -0400

    Created constrcutor from parameters

--- Email_Object.py
+++ Email_Object.py
@@ -6,11 +6,11 @@
 #* Class Declaration for Email Object *#
 
 class Email:
-    def __init__(self):
-        sender = " " # Sender of Email
-        date = " " # Date Email was recieved
-        subject = " " # Subject of Email
-        body = " " # Body of email, should only be 250 characters
+    def __init__(self, sender, date, subject, body):
+        self.sender = sender # Sender of Email
+        self.date = date # Date Email was recieved
+        self.subject = subject # Subject of Email
+        self.body = body # Body of email, should only be 250 characters
 
     def __str__(self):
     	return 'Sender: {0} \nSubject: {1} \nDate: {2} \nBody:\n{3}'\
