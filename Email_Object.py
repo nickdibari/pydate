ee2d8b0f417b77bd33078e6e26523a3bfbfabff7
nickdibari <ndibari@fordham.edu>
2016-10-11 15:24:51 -0400

    Changed wording to avoid using id keyword

--- Email_Object.py
+++ Email_Object.py
@@ -6,12 +6,12 @@
 #* Class Declaration for Email Object *#
 
 class Email:
-    def __init__(self, sender, date, subject, body, ID):
+    def __init__(self, sender, date, subject, body, id_code):
         self.sender = sender # Sender of Email
         self.date = date # Date Email was recieved
         self.subject = subject # Subject of Email
         self.body = body # Body of email
-        self.id = ID # Unique Email ID (use as key)
+        self.id = id_code # Unique Email ID (use as key)
 
     def __str__(self):
     	return 'Sender: {0} \nSubject: {1} \nDate: {2} \nBody:\n{3}'\
