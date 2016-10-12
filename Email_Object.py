d06284d9907913c335c7043bc25b3b61b9023d99
nickdibari <ndibari@fordham.edu>
2016-10-11 23:37:03 -0400

    ID reformatting II

--- Email_Object.py
+++ Email_Object.py
@@ -11,7 +11,7 @@ class Email:
         self.date = date # Date Email was recieved
         self.subject = subject # Subject of Email
         self.body = body # Body of email
-        self.id = id_code # Unique Email ID (use as key)
+        self.id_code = id_code # Unique Email ID (use as key)
 
     def __str__(self):
     	return 'Sender: {0} \nSubject: {1} \nDate: {2} \nBody:\n{3}'\
