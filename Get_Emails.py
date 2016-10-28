41b8036d0cb319e432ba302933e3dc1a423cfcaa
nickdibari <ndibari@fordham.edu>
2016-10-28 12:43:58 -0400

    Changed key for database to be email id code

--- Get_Emails.py
+++ Get_Emails.py
@@ -82,7 +82,7 @@ def Get_Emails(conx):
 # POST: Emails stored in Database 
 def Store_Emails(Target_Emails, db):
     for email in Target_Emails:
-        db[str(datetime.now())] = email # TODO: Consider different key. Maybe include info on sender?        
+        db[email.id_code] = email # TODO: Consider different key. Maybe include info on sender?        
     
 # PRE: List of Email Objects
 # POST: Two list of Email objects
